2003f5698c7449b3f1adcb290b5b658b9b255466
Luca <47627900+Lucaa8@users.noreply.github.com>
2022-05-05 21:31:08 +0200

    Added SFML game loop which updates our current hero on screen.

--- include/Game.h
+++ include/Game.h
@@ -4,13 +4,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <windows.h>
+#include <SFML/Graphics.hpp>
+#include <chrono>
 using namespace std;
+using namespace sf;
 
 #include "World.h"
 #include "Hero.h"
 #include "Warrior.h"
 #include "Wizard.h"
 #include "Necromancer.h"
+#include "Animation.h"
 //#include "Location.h" //already included in World.h or Hero.h
 
 namespace RPG
@@ -20,10 +25,14 @@ namespace RPG
         public:
             Game();
             ~Game();
+            static Game* instance;
             void init();
             void loop();
+            void paint();
             bool isGameRunning() const;
             Hero* getHero(string);
+            void setCurrentHero(Hero* hero);
+            Hero* getCurrentHero() const;
             World* getWorld();
             bool addHero(Hero*);
             bool removeHero(string);
@@ -32,8 +41,11 @@ namespace RPG
         protected:
 
         private:
-            bool isRunning = false;
+            RenderWindow window;
+            Font defaultFont;
+            bool isPaused = false;
             World* world;
+            Hero* currentHero;
             vector<Hero*> entities;
             template <typename T>
             int getIndex(vector<T> vec, T toFind);
--- src/Game.cpp
+++ src/Game.cpp
@@ -2,6 +2,8 @@
 
 namespace RPG
 {
+    Game* Game::instance = nullptr; //must define instance here otherwise throws undefined reference.
+
     Game::Game() {}
 
     Game::~Game(){}
@@ -9,71 +11,83 @@ namespace RPG
     void Game::init()
     {
         world = new World("world", new Location(0.0, 0.0, 0.0));
-        isRunning = true;
+        window.create(VideoMode(1280, 720), "RPG");
+        if (!defaultFont.loadFromFile("./arial.ttf")) //doesnt work when launched from c::b but works when exe is launched manually? wth?..another c::b bullshit ig
+        {
+            cout << "Failed to load default font file" << endl;
+            return;
+        }
     }
 
     void Game::loop()
     {
-        //Test programm for now
-        string cmd = "";
-        cin >> cmd;
-        Hero* h1 = entities.at(0);
-        Hero* h2 = entities.at(1);
-        if(cmd=="i1")
+        //Initial time measurement
+        auto tp = chrono::steady_clock::now();
+
+        while (isGameRunning())
         {
-            h1->interact(*h2);
+            //Game events
+            Event event;
+            while (window.pollEvent(event))
+            {
+                if (event.type == Event::Closed)
+                {
+                    window.close();
+                }
+            }
+
+            //time measurement between each frames, so our hero isn't moving slower if timings between frames are big
+            const auto new_tp = chrono::steady_clock::now();
+            float deltaTime = chrono::duration<float>(new_tp-tp).count();
+            tp = new_tp;
+
+            //Read keyboad and set player direction
+            Vector2f dir = {0.0f, 0.0f};
+            if (Keyboard::isKeyPressed(Keyboard::W))
+            {
+                dir.y = -1.0f;
+            }
+            if(Keyboard::isKeyPressed(Keyboard::S))
+            {
+                dir.y = 1.0f;
+            }
+            if (Keyboard::isKeyPressed(Keyboard::D))
+            {
+                dir.x = 1.0f;
+            }
+            if (Keyboard::isKeyPressed(Keyboard::A))
+            {
+                dir.x = -1.0f;
+            }
+
+            //Erase all the window
+            window.clear();
+
+            //Update our current hero position, motion, etc..
+            currentHero->setDirection(dir); //maybe make one method which set dir and update all in one?
+            currentHero->update(deltaTime);
+            currentHero->draw(window, defaultFont);
+
+            //Re-draw all
+            window.display();
         }
-        else if(cmd=="i2")
-        {
-            h2->interact(*h1);
-        }
-        else if(cmd=="d1")
-        {
-            h1->damage(*h2);
-        }
-        else if(cmd=="d2")
-        {
-            h2->damage(*h1);
-        }
-        else if(cmd=="c")
-        {
-            Wizard* w = (Wizard*)h2;
-            w->castSpeel();
-        }
-        else if(cmd=="s1")
-        {
-            cout << *h1 << endl;
-        }
-        else if(cmd=="s2")
-        {
-            cout << *h2 << endl;
-        }
-        else if(cmd=="tp")
-        {
-            h1->teleport(*h2);
-        }
-        else if(cmd=="spawn")
-        {
-            h1->teleport(world->getSpawnLocation());
-        }
-        else if(cmd=="stop")
-        {
-            isRunning = false;
-        }
-        //read keys, call events, etc..
     }
 
     bool Game::isGameRunning() const
     {
-        return isRunning;
+        return window.isOpen();
     }
 
     bool Game::addHero(Hero* hero)
     {
-        if(getHero(hero->getName())==NULL)
+        if(getHero(hero->getName())==nullptr)
         {
             entities.push_back(hero);
             cout << hero->getName() << " joined the game!" << endl;
+            if(entities.size()==1) //if its the only hero currently in the game, you control it.
+            {
+                setCurrentHero(hero);
+            }
             return true;
         }
         return false;
@@ -82,8 +96,13 @@ namespace RPG
     bool Game::removeHero(string name)
     {
         Hero* h = getHero(name);
-        if(h!=NULL)
+        if(h!=nullptr)
         {
+            if(h==currentHero)
+            {
+                cout << "You can't remove the hero you're playing with now." << endl;
+                return false;
+            }
             entities.erase(entities.begin()+getIndex<Hero*>(entities, h));
             cout << h->getName() << " left the game!" << endl;
             delete h;
@@ -99,7 +118,25 @@ namespace RPG
             if(h->getName()==name)
                 return h;
         }
-        return NULL;
+        return nullptr;
+    }
+
+    void Game::setCurrentHero(Hero* hero)
+    {
+        if(getHero(hero->getName())!=nullptr)
+        {
+            currentHero = hero;
+            cout << "You are now playing with " << hero->getName() << endl;
+        }
+        else
+        {
+            cout << "You can't play with " << hero->getName() << " because she/he isn't in your team!" << endl;
+        }
+    }
+
+    Hero* Game::getCurrentHero() const
+    {
+        return currentHero;
     }
 
     vector<Hero*> Game::getHeroes() const
