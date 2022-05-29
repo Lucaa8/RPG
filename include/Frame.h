6525b96f4f4975e464027ce225a1ad361999fdcb
Luca <47627900+Lucaa8@users.noreply.github.com>
2022-05-29 19:47:02 +0200

    Added methods to encode and decode positions for the sprites into an Integer contained in enum AnimType, Added Dir enum (Direction)

--- include/Frame.h
+++ include/Frame.h
@@ -7,20 +7,74 @@
 using namespace std;
 using namespace sf;
 
+enum AnimType { CASTSPELL=7405632, THRUST=276889664, WALKING=546373696, SLASH=811663424, SHOOT=1087438912, DEATH=1348534336, ATTACK=1415839936 };
+enum Dir { BACK, LEFT, FRONT, RIGHT, NONE=-1 };
+
 class Frame
 {
-    //standard: 64x64
-    //attack:   256x192
+
+    /*int cast =  encode(0, 7, 64, 64);
+    int thrust =  encode(4, 8, 64, 64);
+    int walking = encode(8, 9, 64, 64);
+    int slash =   encode(12, 6, 64, 64);
+    int shoot =   encode(16, 13, 64, 64);
+    int death =   encode(20, 6, 64, 64);
+    int attack =  encode(21, 6, 256, 192);
+
+    cout << cast << endl << thrust << endl << walking << endl << slash << endl << shoot << endl << death << endl << attack << endl;*/
+
+    //7405632
+    //castspell back  : Y=0 Max=7 Size=64x64
+    //castspell left  : Y=1 Max=7 Size=64x64
+    //castspell front : Y=2 Max=7 Size=64x64
+    //castspell right : Y=3 Max=7 Size=64x64
+
+    //276889664
+    //thrust back   : Y=4 Max=8 Size=64x64
+    //thrust left   : Y=5 Max=8 Size=64x64
+    //thrust front  : Y=6 Max=8 Size=64x64
+    //thrust right  : Y=7 Max=8 Size=64x64
+
+    //546373696
+    //walking back   : Y=8  Max=9 Size=64x64
+    //walking left   : Y=9  Max=9 Size=64x64
+    //walking front  : Y=10 Max=9 Size=64x64
+    //walking right  : Y=11 Max=9 Size=64x64
+
+    //811663424
+    //slash back  : Y=12 Max=6 Size=64x64
+    //slash left  : Y=13 Max=6 Size=64x64
+    //slash front : Y=14 Max=6 Size=64x64
+    //slash right : Y=15 Max=6 Size=64x64
+
+    //1087438912
+    //shoot (bow) back  : Y=16 Max=13 Size=64x64
+    //shoot (bow) left  : Y=17 Max=13 Size=64x64
+    //shoot (bow) front : Y=18 Max=13 Size=64x64
+    //shoot (bow) right : Y=19 Max=13 Size=64x64
+
+    //1348534336
+    //death : Y=20 Max=6 Size=64x64
+
+    //1415839936
+    //attack (sword) back  : Y=21 Max=6 Size=256x192 //à voir pour le y de départ sur l'image
+    //attack (sword) left  : Y=22 Max=6 Size=256x192
+    //attack (sword) front : Y=23 Max=6 Size=256x192
+    //attack (sword) right : Y=24 Max=6 Size=256x192
+
     public:
         Frame();
-        Frame(string, string, int, int, int, int);
+        Frame(string, string);
         Frame(const Frame&);
         ~Frame();
         void apply(Sprite&);
         void next();
         string getUid() const;
-        void setFrameX(int);
-        void setFrameY(int);
+        void setCurrent(AnimType);
+        AnimType getCurrent() const;
+        void setDirection(Dir);
+        Dir getDirection() const;
+        void setX(int);
         bool run;
 
     private:
@@ -28,7 +82,13 @@ class Frame
         Texture texture;
         int maxFrames;
         int current;
+        int currentAnimY;
+        AnimType currentAnim;
+        Dir currentDir;
         IntRect** frames;
+        void updateFrames(int, int, int, int);
+        void deleteFrames();
+        void setY(int);
 };
 
 #endif // FRAME_H
--- src/Frame.cpp
+++ src/Frame.cpp
@@ -1,8 +1,24 @@
 #include "Frame.h"
 
-Frame::Frame() : run(false), uid("none"), maxFrames(0), current(0){}
+//max values: 63       63   1024   1024
+int encode(int y, int max, int w, int h)
+{
+    return (y << 26) | (max << 20) | (w << 10) | h;
+}
 
-Frame::Frame(string uid, string texturePath, int maxFrames, int y, int w, int h)
+int* decode(int value)
+{
+    int* values = new int[4];
+    values[0] = (value & 0xFC000000) >> 26;
+    values[1] = (value & 0xF00000) >> 20;
+    values[2] = (value & 0xFFC00) >> 10;
+    values[3] = value & 0x2FF;
+    return values;
+}
+
+Frame::Frame() : run(false), uid("none"), maxFrames(0), current(0), currentAnimY(0){}
+
+Frame::Frame(string uid, string texturePath)
 {
     this->uid = uid;
     if (!texture.loadFromFile(texturePath))
@@ -10,13 +26,12 @@ Frame::Frame(string uid, string texturePath, int maxFrames, int y, int w, int h)
         cout << "Failed to load the " << texturePath << " texture";
         texture.create(64, 64);
     }
-    this->maxFrames = maxFrames;
+    this->maxFrames = -1;
     this->current = 0;
-    this->frames = new IntRect*[maxFrames];
-    for(int i=0;i<maxFrames;i++)
-    {
-        frames[i] = new IntRect(i*w, y*h, w, h);
-    }
+    this->currentAnimY = 0;
+    this->frames = nullptr;
+    this->setCurrent(AnimType::WALKING);
+    this->setDirection(Dir::FRONT);
     this->run = true;
 }
 
@@ -26,6 +41,9 @@ Frame::Frame(const Frame& frame)
     this->texture = frame.texture;
     this->maxFrames = frame.maxFrames;
     this->current = frame.current;
+    this->currentAnimY = frame.currentAnimY;
+    this->currentAnim = frame.currentAnim;
+    this->currentDir = frame.currentDir;
     this->frames = new IntRect*[maxFrames];
     for(int i=0;i<maxFrames;i++)
     {
@@ -37,14 +55,7 @@ Frame::Frame(const Frame& frame)
 
 Frame::~Frame()
 {
-    if(frames!=nullptr)
-    {
-        for(int i=0;i<maxFrames;i++)
-        {
-            delete frames[i];
-        }
-        delete frames;
-    }
+    deleteFrames();
 }
 
 void Frame::apply(Sprite& sprite)
@@ -60,20 +71,94 @@ void Frame::next()
     if(current>=maxFrames)
     {
         current = 0;
+        cout << "next" << endl;
     }
 }
 
-void Frame::setFrameY(int y)
+void Frame::setCurrent(AnimType type)
 {
+    int* decodeType = decode(type);
+    if(decodeType[0] != currentAnimY)
+    {
+        updateFrames(decodeType[1], decodeType[0], decodeType[2], decodeType[3]);
+        this->currentAnim = type;
+        this->currentAnimY = decodeType[0];
+        this->setDirection(this->getDirection()); //needed to update frames Y coord with the same direction as before
+    }
+    delete[] decodeType;
+}
+
+AnimType Frame::getCurrent() const
+{
+    return currentAnim;
+}
+
+void Frame::setDirection(Dir direction)
+{
+    if(direction==-1)
+    {
+        return;
+    }
+    this->currentDir = direction;
+    setY(currentAnimY + direction);
+}
+
+Dir Frame::getDirection() const
+{
+    return currentDir;
+}
+
+void Frame::setX(int x)
+{
+    if(x>=maxFrames)
+        return;
+    this->current = x;
+}
+
+void Frame::setY(int y)
+{
+    if(maxFrames == -1 || frames[0]->top == y*frames[0]->height)
+        return;
     for(int i=0;i<maxFrames;i++)
     {
         frames[i]->top = y*frames[i]->height;
     }
 }
 
-void Frame::setFrameX(int x)
+void Frame::updateFrames(int max, int y, int w, int h)
 {
-    this->current = x;
+    if(this->maxFrames != max)
+    {
+        deleteFrames();
+        this->frames = new IntRect*[max];
+        for(int i=0;i<max;i++)
+        {
+            frames[i] = new IntRect(i*w, y*h, w, h);
+        }
+        this->maxFrames = max;
+        this->current = 0;
+    }
+    else
+    {
+        for(int i=0;i<max;i++)
+        {
+            frames[i]->width  = w;
+            frames[i]->height = h;
+            frames[i]->top = y*h;
+        }
+    }
+}
+
+void Frame::deleteFrames()
+{
+    if(frames!=nullptr)
+    {
+        for(int i=0;i<maxFrames;i++)
+        {
+            delete frames[i];
+        }
+        delete frames;
+    }
 }
 
 string Frame::getUid() const
