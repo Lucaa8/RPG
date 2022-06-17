50981270e794ae6a19acc40a99b6464131fa9c44
Luca DiCosola <luca.dicosola@he-arc.ch>
2022-06-17 11:11:59 +0200

    Loaded map and ĉollisions mask

--- include/World.h
+++ include/World.h
@@ -5,6 +5,17 @@
 using namespace std;
 
 #include "Location.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <windows.h>
+#include <SFML/Graphics.hpp>
+#include <chrono>
+using namespace std;
+using namespace sf;
+
+enum CollideMask { AIR, WALL, HOUSE1, HOUSE2, HOUSE3, POISON, HEAL, SPEED, PV, ARROW };
 
 namespace RPG
 {
@@ -17,12 +28,18 @@ namespace RPG
             void setSpawnLocation(Location* loc);
             void setSpawnLocation(double, double, double);
             Location* getSpawnLocation() const;
+            void initMap();
+            void drawMap(RenderTarget&);
+            int collideWith(RectangleShape&) const;
 
         protected:
 
         private:
             string worldName;
             Location* spawnLocation;
+            Texture gameMapTexture;
+            Sprite gameMap;
+            int mask[24][40];
     };
 }
 
--- src/World.cpp
+++ src/World.cpp
@@ -2,9 +2,28 @@
 
 namespace RPG
 {
+
+    vector<int> splitMaskLine(string line)
+    {
+        vector<int> values;
+        string delim = ",";
+
+        auto start = 0U;
+        auto end = line.find(delim);
+        while (end != string::npos)
+        {
+            values.push_back(stoi(line.substr(start, end - start)));
+            start = end + delim.length();
+            end = line.find(delim, start);
+        }
+        values.push_back(stoi(line.substr(start, end)));
+        return values;
+    }
+
     World::World(string worldName, Location* spawnLoc) : worldName(worldName), spawnLocation(spawnLoc)
     {
         cout << "New world loaded [" << worldName << "] -> Spawn: " << *spawnLoc << endl;
+        initMap();
     }
 
     World::~World()
@@ -32,5 +51,48 @@ namespace RPG
     Location* World::getSpawnLocation() const
     {
         return spawnLocation;
+    }
+
+    void World::initMap()
+    {
+        string texturePath = "textures/map/map.png";
+        if (!gameMapTexture.loadFromFile(texturePath))
+        {
+            cout << "Failed to load the \""+texturePath+"\" texture";
+            gameMapTexture.create(1280, 768);
+        }
+        gameMap.setTexture(gameMapTexture);
+        gameMap.setTextureRect(IntRect(0, 0, 1280, 768));
+        //Collisions
+        string yLine;
+        ifstream collideFile ("textures/map/colisions.txt");
+        if (collideFile.is_open())
+        {
+            int y = 0;
+            while (getline(collideFile,yLine))
+            {
+                int x = 0;
+                for(int i : splitMaskLine(yLine))
+                {
+                    mask[y][x] = i;
+                    x++;
+                }
+                y++;
+            }
+            collideFile.close();
+        }
+    }
+
+    void World::drawMap(RenderTarget& screen)
+    {
+        screen.draw(gameMap);
+    }
+
+    int World::collideWith(RectangleShape& rect) const
+    {
+        int x = rect.getPosition().x/32;
+        int y = rect.getPosition().y/32;
+        //cout << x << " " << y << endl;
+        return -1;
     }
 }
